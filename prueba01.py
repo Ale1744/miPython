4b62edfccd9cf310a6004c263447e160845defc4
alejandra1744 <>
2022-11-25 15:11:28 +0000

    Initial commit

---
+++ prueba01.py
@@ -0,0 +1,18 @@
+enero=300
+febrero=200
+marzo=400
+
+resultado= (enero+febrero+marzo)
+print (resultado)
+
+promedio= (resultado) / 4
+print (promedio)
+
+name = "Ale"
+lastname = "Franchi"
+
+print (name + lastname)
+print (name +" "+ lastname)
+print (type(name))
+print (type (marzo))
+print (type (promedio))
